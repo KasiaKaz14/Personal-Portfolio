aeee5102e804608ff31ed366e3b7f24ae95d1e7e
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2025-01-05 00:32:23 +0100

    added .gitignore

--- contactmail.js
+++ contactmail.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  emailjs.init("7XRoPzNPyeAcpTfc1");
+  fetch("config.json")
+    .then((response) => response.json())
+    .then((config) => {
+      // Inicjalizacja EmailJS z kluczem z pliku konfiguracyjnego
+      emailjs.init(config.user_id);
 
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+      // Obsługa formularza
+      document
+        .getElementById("contact-form")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
 
-      const formData = {
-        from_name: document.getElementById("name").value,
-        from_email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-      };
+          const formData = {
+            from_name: document.getElementById("name").value,
+            from_email: document.getElementById("email").value,
+            message: document.getElementById("message").value,
+          };
 
-      // Wysyłanie wiadomości za pomocą EmailJS
-      emailjs.send("service_k4gnbdn", "template_o4fkvdg", formData).then(
-        function (response) {
-          alert("Wiadomość wysłana pomyślnie!");
-          document.getElementById("contact-form").reset();
-        },
-        function (error) {
-          alert("Nie udało się wysłać wiadomości.");
-          console.log(error);
-        }
-      );
+          emailjs.send(config.service_id, config.template_id, formData).then(
+            function (response) {
+              alert("Wiadomość wysłana pomyślnie!");
+              document.getElementById("contact-form").reset();
+            },
+            function (error) {
+              alert("Nie udało się wysłać wiadomości.");
+              console.log(error);
+            }
+          );
+        });
+    })
+    .catch((error) => {
+      console.error("Błąd w ładowaniu konfiguracji:", error);
     });
 });
